abc07341b0ce45cf38c5dcc79fff0c8f508b43d5
SrivenkatanathanDevOps <srivenkat2206@gmail.com>
2026-03-09 12:19:42 +0530

    Adding bash commands.

---
+++ commands.sh
@@ -0,0 +1,15 @@
+# This is a comment and that will be ignored by the interpreter.
+
+echo Muruga sharanam !
+
+echo This will print #This won't print
+
+#To enable colours in the print we use below commands
+
+echo -e \\e[31Muruga sharanam !]\\e [0m]
+echo -e \\e[32Muruga sharanam !]\\e [0m]
+echo -e \\e[33Muruga sharanam !]\\e [0m]
+echo -e \\e[34Muruga sharanam !]\\e [0m]
+echo -e \\e[35Muruga sharanam !]\\e [0m]
+echo -e \\e[36Muruga sharanam !]\\e [0m]
+
