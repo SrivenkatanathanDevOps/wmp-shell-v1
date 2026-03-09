0c2395f4b0a3c79f4e109af5c916d06380901447
SrivenkatanathanDevOps <srivenkat2206@gmail.com>
2026-03-09 12:24:57 +0530

    Updating the commands scripts

--- commands.sh
+++ commands.sh
@@ -6,10 +6,18 @@ echo This will print #This won't print
 
 #To enable colours in the print we use below commands
 
-echo -e \\e[31Muruga sharanam !]\\e [0m]
-echo -e \\e[32Muruga sharanam !]\\e [0m]
-echo -e \\e[33Muruga sharanam !]\\e [0m]
-echo -e \\e[34Muruga sharanam !]\\e [0m]
-echo -e \\e[35Muruga sharanam !]\\e [0m]
-echo -e \\e[36Muruga sharanam !]\\e [0m]
+echo -e \\e[31mMuruga sharanam !]\\e [0m]
+echo -e \\e[32mMuruga sharanam !]\\e [0m]
+echo -e \\e[33mMuruga sharanam !]\\e [0m]
+echo -e \\e[34mMuruga sharanam !]\\e [0m]
+echo -e \\e[35mMuruga sharanam !]\\e [0m]
+echo -e \\e[36mMuruga sharanam !]\\e [0m]
+
+
+echo -e \\e[41mMuruga sharanam !]\\e [0m]
+echo -e \\e[42mMuruga sharanam !]\\e [0m]
+echo -e \\e[43mMuruga sharanam !]\\e [0m]
+echo -e \\e[44mMuruga sharanam !]\\e [0m]
+echo -e \\e[45mMuruga sharanam !]\\e [0m]
+echo -e \\e[46mMuruga sharanam !]\\e [0m]
 
